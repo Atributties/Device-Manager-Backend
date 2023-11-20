1c74a62f2995d61e8393a8dcef3e87f22e7dbca7
marcusFN <marc599n@stud.kea.dk>
2023-11-20 12:51:10 +0100

    intiation of project

---
+++ src/main/java/com/example/devicemanagerbackend/DeviceManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.devicemanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceManagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devicemanagerbackend/DeviceManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devicemanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
