c2749816791f5c892cced42be3873afb37894e1a
marcusFN <marc599n@stud.kea.dk>
2023-11-21 14:41:12 +0100

    need Mathias fixes but added files

---
+++ src/main/java/com/example/devicemanagerbackend/entities/User.java
@@ -0,0 +1,33 @@
+package com.example.devicemanagerbackend.entities;
+
+import com.example.devicemanagerbackend.enums.UserType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String fullName;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    private String middleName;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+}
