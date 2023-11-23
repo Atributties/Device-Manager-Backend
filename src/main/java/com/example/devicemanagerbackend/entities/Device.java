0ed76357553decd829d9339c62bb4bd05e471732
marcusFN <marc599n@stud.kea.dk>
2023-11-23 10:14:41 +0100

    added Device entity

---
+++ src/main/java/com/example/devicemanagerbackend/entities/Device.java
@@ -0,0 +1,57 @@
+package com.example.devicemanagerbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "unique_imei",
+                        columnNames = "IMEINumber"),
+                @UniqueConstraint(name = "unique_serial_number", columnNames = "SerialNumber")
+        }
+)
+public class Device {
+
+    @Id
+    @Column(name = "device_id")
+    private String id;
+
+    @Column(nullable = false)
+    @Size(min = 15, max = 15)
+    private Long IMEINumber;
+
+    @Column(nullable = false)
+    private String SerialNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DeviceType deviceType;
+
+    @Column(nullable = false)
+    private String deviceModel;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DeviceStatus deviceStatus;
+
+    private String comments;
+
+    @CreationTimestamp //Hibernate automatic create a timestamp in the database
+    private LocalDateTime dateCreated;
+
+    @UpdateTimestamp //Hibernate automatic update this timestamp if there has been implemented a change in the database.
+    private LocalDateTime lastUpdated;
+
+}
+
