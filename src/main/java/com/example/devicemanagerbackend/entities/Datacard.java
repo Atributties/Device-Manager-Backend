8f8c81f68a0f26efda8eb969d2322b942a23b1f4
marcusFN <marc599n@stud.kea.dk>
2023-11-29 10:12:01 +0100

    created Datacard and simcard entities

---
+++ src/main/java/com/example/devicemanagerbackend/entities/Datacard.java
@@ -0,0 +1,27 @@
+package com.example.devicemanagerbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Datacard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "simcard_id")
+    private int id;
+
+    @Column(nullable = false)
+    private String imsinumber;
+
+    @Column(nullable = false)
+    private int pin;
+
+    @Column(nullable = false)
+    private int puk;
+}
