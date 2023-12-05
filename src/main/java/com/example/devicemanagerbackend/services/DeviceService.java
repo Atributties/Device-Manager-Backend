f12f12dc9b4b7c4996933fb7efefa2c9f03d5921
Chrilleweb <christian.munknissen@gmail.com>
2023-12-05 16:06:34 +0100

    IN_STORAGE

--- src/main/java/com/example/devicemanagerbackend/services/DeviceService.java
+++ src/main/java/com/example/devicemanagerbackend/services/DeviceService.java
@@ -2,6 +2,7 @@ package com.example.devicemanagerbackend.services;
 
 import com.example.devicemanagerbackend.entities.Device;
 import com.example.devicemanagerbackend.entities.User;
+import com.example.devicemanagerbackend.enums.DeviceStatus;
 import com.example.devicemanagerbackend.enums.DeviceType;
 import com.example.devicemanagerbackend.exceptions.CustomException;
 import com.example.devicemanagerbackend.repositories.DeviceRepository;
@@ -67,13 +68,14 @@ public class DeviceService {
     public Device assignDeviceToUser(String deviceId, Integer userId) {
         Device device = deviceRepository.findById(deviceId)
                 .orElseThrow(() -> new CustomException("Device not found with id: " + deviceId));
-
         if (userId == null) {
             device.setUser(null);
+            device.setDeviceStatus(DeviceStatus.IN_STORAGE);
         } else {
             User user = userRepository.findById(userId)
                     .orElseThrow(() -> new CustomException("User not found with id: " + userId));
             device.setUser(user); // Assign the device
+            device.setDeviceStatus(DeviceStatus.IN_USE);
         }
 
         return deviceRepository.save(device);
