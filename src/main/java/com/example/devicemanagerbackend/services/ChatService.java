031d97856f8568ecb9184f38f18d8f36fc767057
Chrilleweb <christian.munknissen@gmail.com>
2023-12-07 13:53:32 +0100

    create request

--- src/main/java/com/example/devicemanagerbackend/services/ChatService.java
+++ src/main/java/com/example/devicemanagerbackend/services/ChatService.java
@@ -4,9 +4,11 @@ package com.example.devicemanagerbackend.services;
 import com.example.devicemanagerbackend.DTO.MessageDTO;
 import com.example.devicemanagerbackend.DTO.UserRequestDTO;
 import com.example.devicemanagerbackend.entities.Message;
+import com.example.devicemanagerbackend.entities.User;
 import com.example.devicemanagerbackend.entities.UserRequest;
 import com.example.devicemanagerbackend.exceptions.CustomException;
 import com.example.devicemanagerbackend.repositories.MessageRepository;
+import com.example.devicemanagerbackend.repositories.UserRepository;
 import com.example.devicemanagerbackend.repositories.UserRequestRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +26,13 @@ public class ChatService {
 
     private final UserRequestRepository userRequestRepository;
     private final MessageRepository messageRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public ChatService(UserRequestRepository userRequestRepository, MessageRepository messageRepository) {
+    public ChatService(UserRequestRepository userRequestRepository, MessageRepository messageRepository, UserRepository userRepository) {
         this.userRequestRepository = userRequestRepository;
         this.messageRepository = messageRepository;
+        this.userRepository = userRepository;
     }
 
     public UserRequestDTO createUserRequest(UserRequestDTO userRequestDTO) {
@@ -95,49 +99,65 @@ public class ChatService {
     //Converters
 
     private UserRequestDTO convertUserRequestToDTO(UserRequest userRequest) {
+        List<MessageDTO> messageDTOs = userRequest.getMessages().stream()
+                .map(this::convertMessagesToDTO)
+                .collect(Collectors.toList());
+
         return new UserRequestDTO(
                 userRequest.getId(),
                 userRequest.getRequestType(),
                 userRequest.getUser(),
                 userRequest.getRequestText(),
-                Collections.singletonList(convertMessagesToDTO((Message) userRequest.getMessages())),
+                messageDTOs,
                 userRequest.getDateCreated(),
                 userRequest.getLastUpdated()
         );
     }
 
     private MessageDTO convertMessagesToDTO(Message message) {
-        // Implement the conversion logic from Message to MessageDTO
+        // Convert each Message entity to MessageDTO
         return new MessageDTO(
                 message.getId(),
                 message.getUserRequest(),
                 message.getUser(),
                 message.getMessageText(),
                 message.getDateCreated()
-                // Add other message-related fields as needed
         );
     }
 
     private UserRequest convertToEntity(UserRequestDTO userRequestDTO) {
+        // Fetch the existing User from the database
+        User user = userRepository.findById(userRequestDTO.getUser().getId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + userRequestDTO.getUser().getId()));
+
+        List<Message> messages = Optional.ofNullable(userRequestDTO.getMessages())
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(this::convertMessagesDTOToEntities)
+                .collect(Collectors.toList());
+
+        // Create a new UserRequest with the managed User entity
         return new UserRequest(
                 userRequestDTO.getId(),
                 userRequestDTO.getRequestType(),
-                userRequestDTO.getUser(),
+                user,
                 userRequestDTO.getRequestText(),
-                Collections.singletonList(convertMessagesDTOToEntities((MessageDTO) userRequestDTO.getMessages())),
+                messages,
                 userRequestDTO.getDateCreated(),
                 userRequestDTO.getLastUpdated()
         );
     }
 
     private Message convertMessagesDTOToEntities(MessageDTO messageDTO) {
+        if (messageDTO == null) {
+            return null; // Or handle as needed if null messages are not allowed
+        }
         return new Message(
                 messageDTO.getId(),
                 messageDTO.getUserRequest(),
                 messageDTO.getUser(),
                 messageDTO.getMessageText(),
                 messageDTO.getDateCreated()
-                // Add other message-related fields as needed
         );
     }
 
